6197db820026159d6d8d25fa84db819f2c08718a
Jorge Duarte <jorgeduarte22@gmail.com>
2021-03-02 16:05:44 +0100

    NUEVO SCRIPT PARA HASTAGS

---
+++ actualitzar_hastags.bat
@@ -0,0 +1,12 @@
+@echo off
+echo /********	BUSCAMOS NUEVAS VERSIONES EN EL GIT	********/
+call "C:\Users\Tech\Documents\git_sync.bat"
+echo /********	BUSCAMOS NUEVAS VERSIONES EN EL GIT	********/
+timeout /t 20
+echo /********	INICIAMOS EL SERVIDOR DE SELENIUM EN UNA NUEVA VENTANA	********/
+start cmd.exe /k "java -jar C:\Users\Tech\Documents\Scraper\selenium-server-standalone-3.9.1.jar"
+echo /********	INICIAMOS EL SERVIDOR DE SELENIUM EN UNA NUEVA VENTANA	********/
+timeout /t 60
+echo /********	EJECUTAMOS EL SCRIPT	********/
+php C:\Users\Tech\Documents\Scraper\instagram-hashtags.php
+echo /********	EJECUTAMOS EL SCRIPT	********/
